06ecb1478061c7feb960b0192658c001ee093f9f
Lachlan Campbell <lachlan@hackclub.com>
2020-03-25 17:16:13 -0400

    Fix logos in dark mode

--- components/sponsors.js
+++ components/sponsors.js
@@ -1,39 +1,44 @@
-import { Grid, Link, Image } from 'theme-ui'
+import { Grid, Link, Image, useColorMode } from 'theme-ui'
 
-const list = [
-  'Facebook',
-  'Giphy',
-  'Microsoft',
-  'Pinterest',
-  'Slack',
-  'TikTok',
-  'Twitter',
-  'WeChat'
-]
+const list = {
+  Facebook: true,
+  Giphy: true,
+  Microsoft: true,
+  Pinterest: false,
+  Slack: true,
+  TikTok: true,
+  Twitter: false,
+  WeChat: false
+}
 
-export default () => (
-  <Grid
-    gap={4}
-    sx={{
-      mt: 4,
-      gridTemplateColumns: 'repeat(2, 1fr)',
-      alignItems: 'center'
-    }}
-  >
-    {list.map(name => (
-      <Link
-        href={`https://${name.toLowerCase()}.com`}
-        title={name}
-        key={name}
-        sx={{ lineHeight: 0, width: '100%', maxWidth: '100%' }}
-      >
-        <Image
-          src={`/sponsors/${name.toLowerCase()}.svg`}
-          alt={`${name} logo`}
-          sx={{ height: '100%', maxWidth: '75%', maxHeight: 64 }}
-          loading="lazy"
-        />
-      </Link>
-    ))}
-  </Grid>
-)
+export default () => {
+  const [colorMode] = useColorMode()
+  return (
+    <Grid
+      gap={4}
+      sx={{
+        mt: 4,
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        alignItems: 'center'
+      }}
+    >
+      {Object.keys(list).map(name => (
+        <Link
+          href={`https://${name.toLowerCase()}.com`}
+          title={name}
+          key={name}
+          sx={{ lineHeight: 0, width: '100%', maxWidth: '100%' }}
+        >
+          <Image
+            src={`/sponsors/${name.toLowerCase()}${
+              list[name] && colorMode === 'dark' ? '-white' : ''
+            }.svg`}
+            alt={`${name} logo`}
+            sx={{ height: '100%', maxWidth: '75%', maxHeight: 64 }}
+            loading="lazy"
+          />
+        </Link>
+      ))}
+    </Grid>
+  )
+}
