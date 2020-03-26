86b5d6cbcf2a756ed8b820a908c5020a2ad35aa2
Lachlan Campbell <lachlan@hackclub.com>
2020-03-26 16:20:24 -0400

    Add note about projects

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default () => (
           variant="subtitle"
           sx={{ color: 'muted', mt: 3, mb: 4, fontFamily: 'body' }}
         >
-          Mar 26–30, 2020
+          Mar 26–30, 2020. (Projects will be listed here on Monday.)
         </Heading>
         <Button
           as="a"
