969ffbbc14e15f8fdb8ab39cfd0f4d432732febf
Lachlan Campbell <lachlan@hackclub.com>
2020-03-31 22:45:54 -0400

    Componentize ProjectsGrid

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,6 +1,5 @@
-import { Box, Container, Grid } from 'theme-ui'
-import projects from '../../lib/projects-min.json'
-import ProjectCard from '../../components/project-card'
+import { Box, Container } from 'theme-ui'
+import ProjectsGrid from '../../components/projects-grid'
 
 export default () => (
   <>
@@ -8,11 +7,7 @@ export default () => (
       <h1>Highlighted projects</h1>
     </Box>
     <Container as="main" sx={{ my: [4, 5] }}>
-      <Grid columns={[null, 2]} gap={[3, 4, 5]}>
-        {projects.map(project => (
-          <ProjectCard key={project.id} {...project} />
-        ))}
-      </Grid>
+      <ProjectsGrid />
     </Container>
   </>
 )
