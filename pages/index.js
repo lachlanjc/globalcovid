e8ecc0ea338ca42fda33da08590b0d3b41ba245f
Lachlan Campbell <lachlan@hackclub.com>
2020-03-31 22:05:16 -0400

    Tweak homepage

--- pages/index.js
+++ pages/index.js
@@ -6,64 +6,73 @@ import ProjectCard from '../components/project-card'
 export default () => (
   <>
     <Box
+      as="header"
       sx={{
         bg: 'sheet',
         pt: 3,
-        px: 3,
-        pb: [3, 4, 5],
+        pb: [4, 5],
         mb: [4, 5],
-        p: { maxWidth: 'copy', fontSize: [1, 2], ':last-of-type': { mb: 0 } }
+        'h1 ~ p': { maxWidth: 'copy', fontSize: 2, ':last-of-type': { mb: 0 } }
       }}
     >
       <Container>
+        <Text
+          as="p"
+          variant="subtitle"
+          sx={{
+            color: 'muted',
+            fontSize: [2, 3],
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            mt: 3
+          }}
+        >
+          Mar 26–30, 2020
+        </Text>
         <Heading
           as="h1"
           sx={{
             variant: 'text.title',
             fontFamily: 'heading',
-            maxWidth: 'copyPlus',
-            fontSize: [3, 4, 5],
             mt: 0,
-            span: {
+            mb: 4,
+            '> span': {
               display: 'block',
-              color: 'accent',
-              fontSize: [3, 5, 6],
-              mb: 3
+              color: 'primary',
+              fontSize: [2, 4, 5, 7],
+              mb: 2,
+              ':before': { content: '"#"', color: 'accent' },
+              span: { mx: 1 }
+            },
+            strong: {
+              color: 'blue',
+              maxWidth: 'copyPlus',
+              fontSize: [5, 6]
             }
           }}
         >
-          <span>#BuildforCOVID19</span> Global Online Hackathon
-        </Heading>
-        <Heading
-          as="h2"
-          variant="subtitle"
-          sx={{ color: 'muted', mt: 3, mb: 4, fontFamily: 'body' }}
-        >
-          Mar 26–30, 2020.
+          <span>
+            Build<span>for</span>COVID19
+          </span>
+          <strong>Global Online Hackathon</strong>
         </Heading>
         <About />
       </Container>
     </Box>
-    <Container sx={{ mb: [5, 6] }}>
-      <Heading
-        sx={{
-          variant: 'text.title',
-          fontFamily: 'heading',
-          maxWidth: 'copyPlus',
-          fontSize: [3, 4, 5],
-          m: 0
-        }}
-      >
+    <Container as="article" sx={{ py: [3, 4], mb: [5, 6] }}>
+      <Heading sx={{ variant: 'text.title', fontSize: [4, 5], m: 0 }}>
         Highlighted projects
       </Heading>
-      <Text sx={{ fontSize: [1, 2], mt: [3, 4], mb: [4, 5], maxWidth: 'copy' }}>
+      <Text
+        sx={{ fontSize: [2, 3], mt: [3, 4], mb: [4, 5], maxWidth: 'copyPlus' }}
+      >
         We consider every participant who dedicated their time and skills to
         #BuildforCOVID19 a winner. That said, our team of health & technical
         expert judges have spent the past week considering the viability,
         potential and scalability of submissions to land on the following
         highlighted projects.
       </Text>
-      <Grid columns={[null, 2]} gap={[3, 4, 5]}>
+      <Grid columns={[null, 2]} gap={[4, 5]} sx={{ mx: [-3, 0] }}>
         {projects.map(project => (
           <ProjectCard key={project.name} {...project} />
         ))}
